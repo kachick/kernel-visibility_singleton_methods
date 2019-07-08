632ffbe85434c73d6fa1548ab094fdaed3c9f6c6
Kenichi Kamiya <kachick1@gmail.com>
2019-07-08 16:06:45 +0900

    Drop ruby 1.8.7 support

--- lib/kernel/visibility_singleton_methods.rb
+++ lib/kernel/visibility_singleton_methods.rb
@@ -5,24 +5,22 @@
 module Kernel
 
   # @note
-  # * Use (class << self; self; end) idiom for to get singleton classies.
-  #   Because 1.8.7 dosen't have Kernel#singleton_class. 
   # * include_inherited option is not supported yet.
   #   Because it is too difficult in PureRuby :<
 
-  # @return [Array<Symbol, String>] 1.8.7: Array<String>, 1.9~: Array<Symbol>
+  # @return [Array<Symbol>]
   def public_singleton_methods
-    (class << self; self; end).public_instance_methods false
+    singleton_class.public_instance_methods false
   end
 
-  # @return [Array<Symbol, String>] 1.8.7: Array<String>, 1.9~: Array<Symbol>
+  # @return [Array<Symbol>]
   def protected_singleton_methods
-    (class << self; self; end).protected_instance_methods false
+    singleton_class.protected_instance_methods false
   end
 
-  # @return [Array<Symbol, String>] 1.8.7: Array<String>, 1.9~: Array<Symbol>
+  # @return [Array<Symbol>]
   def private_singleton_methods
-    (class << self; self; end).private_instance_methods false
+    singleton_class.private_instance_methods false
   end
 
 end
